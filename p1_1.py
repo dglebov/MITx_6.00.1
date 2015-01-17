ee10ef5686d60de49e06d5356d31e92b7f30ea11
Dmitriy <glebovds@Dmitriys-MBP.homelan>
2015-01-17 22:01:34 +0300

    MITx_6.00.1

---
+++ p1_1.py
@@ -0,0 +1,9 @@
+
+s = 'azcbobobegghakl'                 
+
+
+vowels = ('a','e','i','o','u')
+numberOfVowels = 0
+for char in s:
+    if char in vowels : numberOfVowels += 1
+print 'Number of vowels: %d' % numberOfVowels
